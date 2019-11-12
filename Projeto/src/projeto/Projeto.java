66182d97d4cc9072e359835f1d7334239c79ea61
levi <levi@manjarolevi>
2019-11-12 19:28:28 -0200

    teste commit

--- Projeto/src/projeto/Projeto.java
+++ Projeto/src/projeto/Projeto.java
@@ -5,6 +5,11 @@
  */
 package projeto;
 
+import java.io.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author levi
@@ -15,7 +20,9 @@ public class Projeto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("oi");
+        System.out.println("haha\n");
     }
+    
+    
     
 }
---
+++ Projeto/src/projeto/reading/Reading.java
@@ -0,0 +1,33 @@
+package projeto.reading;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author levi
+ */
+public class Reading {
+    public static String readFile(String locationString){
+        String content = "";
+        String line;
+        String separator = "$";
+        try {
+            File file = new File("/home/levi/NetBeansProjects/CC_2_ANO_TRABALHO_AED_4/arquivo.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader fileBuffer = new BufferedReader(fileReader);
+            
+            line = fileBuffer.readLine();
+            while(line != null){  
+                content += line;
+                content += separator;
+                line = fileBuffer.readLine();                    
+            }            
+        } catch (IOException e) {
+            System.err.println("Erro na Leitura de Arquivo");
+        }
+        return content;
+    }
+}
