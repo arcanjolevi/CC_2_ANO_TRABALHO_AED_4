25ced806f0a8baca2770452f295f7605871002d4
levi <levi@manjarolevi>
2019-11-13 20:03:05 -0200

    Primeiro commit

---
+++ src/reading/Reading.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reading;
+
+import java.io.*;
+
+/**
+ *
+ * @author levi
+ */
+public class Reading {
+    public static String[] readFile(String locationString){
+        String content = "";
+        String line;
+        String separator = "$";
+        String[] aux;
+        try {
+            File file = new File(locationString);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader fileBuffer = new BufferedReader(fileReader);
+
+            line = fileBuffer.readLine();
+            while(line != null){ 
+                aux = line.split(" ");
+                for(String a: aux)
+                    content += a;                
+                content += separator;
+                line = fileBuffer.readLine();                    
+            }            
+        } catch (IOException e) {
+            System.err.println("Erro na leitura de arquivo");
+        }
+        return content.split(separator);
+    }
+}
